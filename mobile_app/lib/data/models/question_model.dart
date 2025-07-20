6c18cc3f6aee2e2aea517a80ab3f3304ed804bdc
Vivek5kwt <vivek5kwt@gmail.com>
2025-07-20 08:03:22 +0530

    Add accepted value handling

--- mobile_app/lib/data/models/question_model.dart
+++ mobile_app/lib/data/models/question_model.dart
@@ -1,6 +1,15 @@
 class QuestionModel {
   final String variableNumber;
   final String questionText;
+  final double finalValue;
 
-  QuestionModel({required this.variableNumber, required this.questionText});
+  QuestionModel({
+    required this.variableNumber,
+    required this.questionText,
+    required this.finalValue,
+  });
+
+  /// Accepted value for the question. If [finalValue] is negative,
+  /// this returns `0`, otherwise it returns [finalValue].
+  double get acceptedValue => finalValue < 0 ? 0 : finalValue;
 }
